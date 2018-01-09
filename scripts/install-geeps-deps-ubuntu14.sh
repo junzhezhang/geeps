483c4fd33ed35c78d77e84f3735d15e9dd9a298a
junzhezhang <30921847+junzhezhang@users.noreply.github.com>
2018-01-09 14:14:30 +0800

    remove sudo

--- scripts/install-geeps-deps-ubuntu14.sh
+++ scripts/install-geeps-deps-ubuntu14.sh
@@ -1,10 +1,10 @@
-sudo apt-get update
-sudo apt-get install -y --force-yes libboost-thread-dev
-sudo apt-get install -y --force-yes libboost-program-options-dev
-sudo apt-get install -y --force-yes libgoogle-glog-dev
-sudo apt-get install -y --force-yes libgflags-dev
-sudo apt-get install -y --force-yes libatlas-base-dev
-sudo apt-get install -y --force-yes libtbb-dev
-sudo apt-get install -y --force-yes libzmq-dev
-sudo apt-get install -y --force-yes scons
-sudo apt-get install -y --force-yes pdsh
+apt-get update
+apt-get install -y --force-yes libboost-thread-dev
+apt-get install -y --force-yes libboost-program-options-dev
+apt-get install -y --force-yes libgoogle-glog-dev
+apt-get install -y --force-yes libgflags-dev
+apt-get install -y --force-yes libatlas-base-dev
+apt-get install -y --force-yes libtbb-dev
+apt-get install -y --force-yes libzmq-dev
+apt-get install -y --force-yes scons
+apt-get install -y --force-yes pdsh
